fc507f02b5987922d591b802c696767617102c29
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-10-14 15:52:00 +1300

    Initial boilerplate.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(name='pmr2.omex',
+      version=version,
+      description="PMR2 COMBINE Archive support",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Tommy Yu',
+      author_email='tommy.yu@auckland.ac.nz',
+      url='https://github.com/PMR2/pmr2.omex/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['pmr2'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
